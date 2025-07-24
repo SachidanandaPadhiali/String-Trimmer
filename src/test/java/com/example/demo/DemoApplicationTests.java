d6835a435e60e8933fce1dba0db24fba3915a66d
Sachidananda Padhiali <sagarpadhiali99@gmail.com>
2025-07-24 17:30:47 +0530

    Test Updates

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,25 +22,18 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	void validInput_shouldReturnTrimmedString() throws Exception {
-		mockMvc.perform(get("/api/remove").param("input", "eloquent"))
+	@ParameterizedTest
+	@CsvSource({
+			"eloquent,loquen",
+			"country,ountr",
+			"person,erso",
+			"xyz,y",
+			"_123_%qwerty+,123_%qwerty"
+	})
+	void inputWithThreeOrMoreCharacters_shouldReturnTrimmedString(String input, String expected) throws Exception {
+		mockMvc.perform(get("/api/remove").param("input", input))
 				.andExpect(status().isOk())
-				.andExpect(content().string("loquen"));
-	}
-
-	@Test
-	void input_country_shouldReturn_ountr() throws Exception {
-		mockMvc.perform(get("/api/remove").param("input", "country"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("ountr"));
-	}
-
-	@Test
-	void input_person_shouldReturn_erso() throws Exception {
-		mockMvc.perform(get("/api/remove").param("input", "person"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("erso"));
+				.andExpect(content().string(expected));
 	}
 
 	@Test
@@ -49,20 +44,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	void input_xyz_shouldReturn_y() throws Exception {
-		mockMvc.perform(get("/api/remove").param("input", "xyz"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("y"));
-	}
-
-	@Test
 	void singleCharacterInput_shouldReturnBadRequest() throws Exception {
-		mockMvc.perform(get("/api/remove").param("input", "a"))
-				.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	void encodedSpecialCharacters_shouldBeHandledCorrectly() throws Exception {
 		mockMvc.perform(get("/api/remove").param("input", "_123_%qwerty+"))
 				.andExpect(status().isOk())
 				.andExpect(content().string("123_%qwerty"));
